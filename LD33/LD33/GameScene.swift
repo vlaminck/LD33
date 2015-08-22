2d9bb6224fa38f19d14dc6168513d4996bfa8707
vlaminck <steve@vlaminck.com>
2015-08-22 11:51:20 -0500

    Added Space and Spaceship

--- LD33/LD33/GameScene.swift
+++ LD33/LD33/GameScene.swift
@@ -9,37 +9,67 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let playerMovePointsPerSec: CGFloat = 800.0
+    var lastUpdateTime: NSTimeInterval = 0
+    var dt: NSTimeInterval = 0
+    var velocity = CGPointZero
+    var lastTouchLocation: CGPoint?
+    
+    let player: SKSpriteNode = SKSpriteNode(imageNamed: "Spaceship")
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        player.position = CGPoint(x: 200, y: size.height / 2)
+        player.zRotation = CGFloat(-M_PI_2)
+        player.xScale = 0.25
+        player.yScale = 0.25
+        addChild(player)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        sceneTouched(touches.first)
     }
-   
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        sceneTouched(touches.first)
+    }
+    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        
+        if let lastTouch = lastTouchLocation {
+            let diff = lastTouch - player.position
+            if (diff.length() <= playerMovePointsPerSec * CGFloat(dt)) {
+                player.position = lastTouchLocation!
+                velocity = CGPointZero
+            } else {
+                moveSprite(player, velocity: velocity)
+            }
+        }
+        
     }
+    
+    func sceneTouched(touch: UITouch?) {
+        guard let touch = touch else { return }
+        let location = touch.locationInNode(self)
+        let offset = location - player.position
+        let direction = offset.normalized()
+        velocity = direction * playerMovePointsPerSec
+        lastTouchLocation = location
+    }
+    
+    func moveSprite(sprite: SKSpriteNode, velocity: CGPoint) {
+        let amountToMove = velocity * CGFloat(dt)
+        sprite.position += amountToMove
+    }
+    
 }
